1e74a97d2a98946e5454f0ba2bec64ade2c5538a
ItsMario25 <mmariohernandez58@gmail.com>
2023-04-03 15:09:19 -0500

    modified:   handlers/handler.go 	modified:   routers/routers.go 	new file:   templates/index.html 	modified:   templates/login.html 	new file:   templates/registro.html

--- handlers/handler.go
+++ handlers/handler.go
@@ -1,11 +1,6 @@
 package handlers
 
 import (
-	"log"
-	"net/http"
-	"os"
-	"path/filepath"
-
 	"webservice/middlewares"
 	"webservice/routers"
 
@@ -14,28 +9,10 @@ import (
 
 func InitRoutes() *gin.Engine {
 	r := gin.Default()
-	// Define una ruta para la página de inicio
-	r.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "Bienvenido al servidor de mi-paquete",
-		})
-	})
-	// Ruta para la página de login
-	r.GET("/login", func(c *gin.Context) {
-		dir, err := os.Getwd()
-		if err != nil {
-			log.Fatal(err)
-		}
-		filepath := filepath.Join(dir, "templates", "login.html")
-		fileContent, err := os.ReadFile(filepath)
-		if err != nil {
-			c.String(http.StatusInternalServerError, "Error al leer archivo: "+err.Error())
-			return
-		}
-		// Renderizar contenido del archivo en HTML
-		c.Data(http.StatusOK, "text/html; charset=utf-8", fileContent)
-	})
 
+	r.GET("/", routers.GetLogin)
+	r.GET("/login", routers.GetLogin)
+	r.GET("/registro", routers.GetRegistro)
 	r.POST("/registro", middlewares.ChequeoBD(), routers.Registro)
 
 	return r
--- routers/routers.go
+++ routers/routers.go
@@ -1,7 +1,10 @@
 package routers
 
 import (
+	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 
 	"webservice/models"
 
@@ -11,6 +14,7 @@ import (
 /*Registro es la funcion para crear en la BD el registro de usuario */
 func Registro(c *gin.Context) {
 	var t models.Usuario
+
 	if err := c.Bind(&t); err != nil {
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{
 			"error": "Error en los datos recibidos " + err.Error(),
@@ -33,6 +37,37 @@ func Registro(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusCreated, gin.H{
-		"message": "Usuario registrado exitosamente",
+		"message":  "Usuario registrado exitosamente",
+		"Usuario":  t.Nombre,
+		"Password": t.Password,
+		"Email":    t.Email,
 	})
+}
+
+func GetLogin(c *gin.Context) {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	filepath := filepath.Join(dir, "templates", "login.html")
+	fileContent, err := os.ReadFile(filepath)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Error al leer archivo: "+err.Error())
+		return
+	}
+	c.Data(http.StatusOK, "text/html; charset=utf-8", fileContent)
+}
+
+func GetRegistro(c *gin.Context) {
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+	filepath := filepath.Join(dir, "templates", "registro.html")
+	fileContent, err := os.ReadFile(filepath)
+	if err != nil {
+		c.String(http.StatusInternalServerError, "Error al leer archivo: "+err.Error())
+		return
+	}
+	c.Data(http.StatusOK, "text/html; charset=utf-8", fileContent)
 }
