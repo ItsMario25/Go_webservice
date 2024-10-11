d8ec2793124dd6101bc75060bdb3b39e2b62e19d
ItsMario25 <mmariohernandez58@gmail.com>
2024-10-10 19:07:46 -0500

    cambios reporte bd

--- bd/criterios_evl.go
+++ bd/criterios_evl.go
@@ -10,21 +10,24 @@ func Get_criterios_bd(tipo string) ([]core.Criterios, error) {
 
 	if tipo == "estudiante" {
 		formato := "F02"
-		if err := db.Select("nombre_criterio").Where("id_formato = ? ", formato).Find(&criterio).Error; err != nil {
+		if err := db.Select("nombre_criterio").Where("id_formato = ? ", formato).Order("id_criterio ASC").
+			Find(&criterio).Error; err != nil {
 			return []core.Criterios{}, err
 		} else {
 			return criterio, nil
 		}
 	} else if tipo == "docente" {
 		formato := "F03"
-		if err := db.Select("nombre_criterio").Where("id_formato = ? ", formato).Find(&criterio).Error; err != nil {
+		if err := db.Select("nombre_criterio").Where("id_formato = ?", formato).Order("id_criterio ASC").
+			Find(&criterio).Error; err != nil {
 			return []core.Criterios{}, err
 		} else {
 			return criterio, nil
 		}
 	} else {
 		formato := "F01"
-		if err := db.Select("nombre_criterio").Where("id_formato = ? ", formato).Find(&criterio).Error; err != nil {
+		if err := db.Select("nombre_criterio").Where("id_formato = ? ", formato).Order("id_criterio ASC").
+			Find(&criterio).Error; err != nil {
 			return []core.Criterios{}, err
 		} else {
 			return criterio, nil
